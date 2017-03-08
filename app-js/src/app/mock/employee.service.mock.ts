ab467c1dfde1d6ff261e1c2af0d2057aa6f0583d
fabm <francisco.ab.monteiro@gmail.com>
2017-03-08 14:50:43 +0000

    Update employee.service.mock.ts
    refactoring

--- app-js/src/app/mock/employee.service.mock.ts
+++ app-js/src/app/mock/employee.service.mock.ts
@@ -6,60 +6,56 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class EmployeeServiceMock extends EmployeeService {
 
+    private pages = [
+        {
+            nextPageToken: '1',
+            employees: [
+                { id: 0, age: 30, name: 'Francisco', salary: 3000 },
+                { id: 1, age: 31, name: 'Bernardo', salary: 2000 }
+            ]
+        },
+        {
+            nextPageToken: '2',
+            employees: [
+                { id: 2, age: 32, name: 'Manuel', salary: 3500 },
+                { id: 3, age: 33, name: 'António', salary: 2500 }
+            ]
+        },
+        {
+            nextPageToken: null,
+            employees: [
+                { id: 4, age: 34, name: 'José', salary: 4000 },
+                { id: 5, age: 35, name: 'Miguel', salary: 4500 }
+            ]
+        }
+    ];
+
     constructor() {
         super();
     }
 
     load(pageToken?: string): Observable<EmployeePage> {
-        if (pageToken == null) {
-            return new Observable(observer => {
-                setTimeout(() => {
-                    observer.next({
-                        nextPageToken: '123',
-                        employees: [
-                            { id: 0, age: 30, name: 'Francisco', salary: 3000 },
-                            { id: 1, age: 31, name: 'Bernardo', salary: 2000 }
-                        ]
-                    });
-                }, 200);
-            });
-        } else if (pageToken == '123') {
-            return new Observable(observer => {
-                setTimeout(() => {
-                    observer.next({
-                        nextPageToken: 345,
-                        employees: [
-                            { id: 2, age: 32, name: 'Manuel', salary: 3500 },
-                            { id: 3, age: 33, name: 'António', salary: 2500 }
-                        ]
-                    });
-                }, 200);
-            });
-        } else if (pageToken == '345') {
-            return new Observable(observer => {
-                setTimeout(() => {
-                    observer.next({
-                        nextPageToken: null,
-                        employees: [
-                            { id: 4, age: 34, name: 'José', salary: 4000 },
-                            { id: 5, age: 35, name: 'Miguek', salary: 4500 }
-                        ]
-                    });
-                }, 200);
-            });
-        }
+        let pageMap = {
+            null: 0,
+            '1': 1,
+            '2': 2
+        };
+
+        return new Observable(observer => {
+            setTimeout(() => observer.next(this.pages[pageMap[pageToken]]), 200);
+        });
     }
 
     insert(employe: Employee): Observable<any> {
-        return new Observable(observer => observer.next());
+        return new Observable(observer => observer.complete());
     }
 
     update(employe: Employee): Observable<any> {
-        return new Observable(observer => observer.next());
+        return new Observable(observer => observer.complete());
     }
 
     delete(id: number): Observable<any> {
-        return new Observable(observer => observer.next());
+        return new Observable(observer => observer.complete());
     }
 
 }
