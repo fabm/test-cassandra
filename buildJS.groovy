03da0e424bf70e67ae2186200d04c46d654ab208
fabm <francisco.ab.monteiro@gmail.com>
2017-03-17 14:34:03 +0000

    Update buildJS.groovy
    preinstall script

--- buildJS.groovy
+++ buildJS.groovy
@@ -1,29 +1,41 @@
-import org.apache.commons.exec.*;
+import org.apache.commons.exec.*
 
-CommandLine command = new CommandLine('ng');
-command.addArgument('build')
-command.addArgument('--prod')
-PumpStreamHandler streamHandler = new PumpStreamHandler();
-DefaultExecutor executor = new DefaultExecutor();
-executor.setWorkingDirectory(new File("./app-js"));
-executor.setStreamHandler(streamHandler);
-executor.execute(command);
+def executeNodeCmd = { List args ->
 
+    CommandLine command = new CommandLine('node')
+    args.each { arg ->
+        command.addArgument(arg.toString())
+    }
+    PumpStreamHandler streamHandler = new PumpStreamHandler();
+    DefaultExecutor executor = new DefaultExecutor();
+    executor.setWorkingDirectory(new File("./app-js"));
+    executor.setStreamHandler(streamHandler);
+    executor.execute(command);
+}
+
+def executeNgCli = { List args ->
+    List<Object> cargs = ["${project.basedir.absolutePath}/app-js/node_modules/@angular/cli/bin/ng"]
+    cargs.addAll(args)
+    executeNodeCmd(cargs)
+}
+
+if (!("${project.basedir.absolutePath}/app-js/node_modules" as File).exists()) {
+    executeNodeCmd(["preinstall.js"])
+}
+executeNgCli(['build', '--prod'])
 def srcPath = "${project.basedir.absolutePath}/app-js/dist"
 def desPath = "${project.basedir.absolutePath}/target/classes/static"
 
-
-
 ant.sequential {
-	echo("delete $desPath")
-	delete(dir:desPath, quiet:true, verbose:false)
-	echo 'done'
-	echo("from $srcPath")
-	mkdir(dir:desPath)
-	copy(todir: desPath) {
-		fileset(dir: srcPath) {
-			include(name: "**/*")
-		}
-	}
-	echo("done")
+    echo("delete $desPath")
+    delete(dir: desPath, quiet: true, verbose: false)
+    echo 'done'
+    echo("from $srcPath")
+    mkdir(dir: desPath)
+    copy(todir: desPath) {
+        fileset(dir: srcPath) {
+            include(name: "**/*")
+        }
+    }
+    echo("done")
 }
